e062192aa32479c3de1e7c37ed7d765b761d8ccb
Resham01005 <mishal.resham002@gmail.com>
2025-03-24 23:54:53 +0530

    Update DisplayHome.jsx

--- spotify-clone/src/components/DisplayHome.jsx
+++ spotify-clone/src/components/DisplayHome.jsx
@@ -21,7 +21,7 @@ const DisplayHome = () => {
     <div className='mb-4'>
       <h1 className='my-5 font-bold text-2xl'>Today's biggest hits</h1>
       <div className='flex overflow-auto'>
-     {songsData.map((item,index)=>(<SongItem key={index} name={item.name} desc={item.desc} id={item._id} image={item.image}/>))}
+     {songsData.slice(0,6).map((item,index)=>(<SongItem key={index} name={item.name} desc={item.desc} id={item._id} image={item.image}/>))}
       </div>
     </div>
     </>
